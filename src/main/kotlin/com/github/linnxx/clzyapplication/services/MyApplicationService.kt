c1643fc5387fff44925647c8e2ceb4bf57fc20d7
GitHub Action <action@github.com>
2021-10-26 12:58:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/linnxx/clzyapplication/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linnxx/clzyapplication/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linnxx.clzyapplication.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linnxx.clzyapplication.MyBundle
 
 class MyApplicationService {
 
